fceaf458f4144dca8726d7bb3a02951394c4a5b0
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-14 20:33:04 -0500

    feat: roll dice button gets random dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,16 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
     @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
 //        change 1st dice image to dice with 6 dots
         diceImageView1.image = UIImage(named: "DiceSix")
-        // change transparency of the 1st dice
-        diceImageView1.alpha = 0.5
         // change the 2nd dice to the dice with 2 dots
         diceImageView2.image = UIImage(named: "DiceTwo")
+        
+        // change transparency of the 1st dice
+//        diceImageView1.alpha = 0.5
+        
+        
+    }
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        diceImageView1.image = UIImage(named: diceNames.randomElement()!)
+        diceImageView2.image = UIImage(named: diceNames.randomElement()!)
     }
 
 
