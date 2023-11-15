119b9d2b82708a666dd0e754d7c90b5fb7fe56e4
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-15 17:59:13 -0500

    start up dices are randomized

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,14 +14,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let diceNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // Start up as random dice
+        diceImageView1.image = UIImage(named: diceNames.randomElement()!)
+        diceImageView2.image = UIImage(named: diceNames.randomElement()!)
+        
 //        change 1st dice image to dice with 6 dots
-        diceImageView1.image = UIImage(named: "DiceSix")
+//        diceImageView1.image = UIImage(named: "DiceSix")
         // change the 2nd dice to the dice with 2 dots
-        diceImageView2.image = UIImage(named: "DiceTwo")
+//        diceImageView2.image = UIImage(named: "DiceTwo")
         
         // change transparency of the 1st dice
 //        diceImageView1.alpha = 0.5
@@ -29,7 +35,7 @@ class ViewController: UIViewController {
         
     }
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        let diceNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        
         diceImageView1.image = UIImage(named: diceNames.randomElement()!)
         diceImageView2.image = UIImage(named: diceNames.randomElement()!)
     }
